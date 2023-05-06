e7a69923dabe186415b445816cbabef7af5e87f8
ulysses316 <ulises316@live.com.mx>
2023-05-05 23:34:11 -0600

    Codigo on hold para esperar las formulas

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 const IMCButton = document.querySelector('#submit');
+const IMCValueBe = document.querySelector('#imc-value-benedict');
+const IMCValueMi = document.querySelector('#imc-value-mifflin');
+const benedict = document.querySelector('#value-benedict');
+const mifflin = document.querySelector('#value-mifflin');
+const ETABennedict = document.querySelector('#eta-value-benedict');
+const ETAMifflin = document.querySelector('#eta-value-mifflin');
+const FAFBennedict = document.querySelector('#faf-value-benedict');
+const FAFMifflin = document.querySelector('#faf-value-mifflin');
 
-const calcIMC = (weight, size)=>{
-    return weight / ((size/100)*(size/100));
+
+const calcIMC = (weight, size) => {
+    return weight / ((size / 100) * (size / 100));
 }
 
-IMCButton.addEventListener( 'click', ()=>{
-    const age = document.querySelector('#age').value;
-    const size = document.querySelector('#size').value;;
-    const weight = document.querySelector('#weight').value;;
-    const IMC = weight / ((size/100)*(size/100));
-    let gender = document.querySelectorAll('input[name="gender"]');
-    gender = (gender[0].checked ? gender[0].value : gender[1].value)
+const calcBenedict = (imc, gender) => { }
+const calcMifflin = (imc, gender) => { }
+const calcETA = () => { }
+const calcFAF = () => { }
 
+IMCButton.addEventListener('click', () => {
+    const age = document.querySelector('#age').value;
+    const size = document.querySelector('#size').value;
+    const weight = document.querySelector('#weight').value;
+    const IMC = weight / ((size / 100) * (size / 100));
+    let gender = document.querySelectorAll('input[name="gender"]');
+    gender = (gender[0].checked ? gender[0].value : gender[1].value);
+    const faf = document.querySelectorAll('#physical-activity')[0].value;
+    
     // variales de resultado
-    const IMCValueBe = document.querySelector('#imc-value-benedict');
-    const IMCValueMi = document.querySelector('#imc-value-mifflin');
     IMCValueBe.innerHTML = IMC.toFixed(2);
     IMCValueMi.innerHTML = IMC.toFixed(2);
-    
+
 })
